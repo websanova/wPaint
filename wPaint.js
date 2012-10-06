d7dcf8134c2d3c9ca612a98e2e0b7dbc71f8f6d0
websanova <rob@websanova.com>
2012-10-06 15:10:55 -0400

    Added mobile support.

--- wPaint.js
+++ wPaint.js
@@ -8,7 +8,7 @@
  * @license         This wPaint jQuery plug-in is dual licensed under the MIT and GPL licenses.
  * @link            http://www.websanova.com
  * @docs            http://www.websanova.com/plugins/websanova/paint
- * @version         Version 1.3.1
+ * @version         Version 1.3.2
  *
  ******************************************/
 (function($)
@@ -212,7 +212,31 @@
 				}
 			});
 			
+			this.bindMobile();
+
 			return $(this.canvas);
+		},
+
+		bindMobile: function()
+		{
+			$(this.canvas).bind('touchstart touchmove touchend touchcancel', function ()
+			{
+				var touches = event.changedTouches, first = touches[0], type = ""; 
+
+				switch (event.type)
+				{
+					case "touchstart": type = "mousedown"; break; 
+					case "touchmove": type = "mousemove"; break; 
+					case "touchend": type = "mouseup"; break; 
+					default: return;
+				}
+
+				var simulatedEvent = document.createEvent("MouseEvent"); 
+
+				simulatedEvent.initMouseEvent(type, true, true, window, 1, first.screenX, first.screenY, first.clientX, first.clientY, false, false, false, false, 0/*left*/, null);
+				first.target.dispatchEvent(simulatedEvent);
+				event.preventDefault();
+			});
 		},
 		
 		generateTemp: function()
